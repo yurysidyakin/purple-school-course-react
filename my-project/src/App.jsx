856bd4373eb93d5847fd732ed03a91991f2a8916
fikyslsgg <yurafikys@gmail.com>
2023-12-13 22:29:00 +0300

    feat: completed the task #1 (components)

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
+import Button from './components/Button/Button';
+import Title from './components/Title/Title';
+import Text from './components/Text/Text';
 
 function App() {
+  const text = [
+    'Введите название фильма, сериала или мультфильма для поиска и добавления в избранное.'
+  ];
+  const title = ['Поиск'];
   return (
     <>
-      <div>Project</div>
+      <Title title={title[0]} />
+      <Text text={text[0]} />
+      <Button />
     </>
   );
 }
---
+++ my-project/src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import './Button.css';
+
+function Button() {
+  return <button className="button">Искать</button>;
+}
+
+export default Button;
