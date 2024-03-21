5243b9c4cd2e1668459924df2317f6d98e287c93
Yury Sidyakin <yurafikys@gmail.com>
2024-03-21 22:43:01 +0300

    feat: Добавил ElementError

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './index.css';
-import { Layout } from './layouts/Menu/Layout';
-import { Error } from './pages/Error/Error';
-import { Favorites } from './pages/Favorites/Favorites';
-import { Login } from './pages/Login/Login';
-import { Menu } from './pages/Menu/Menu';
-import { Movie } from './pages/Movie/Movie';
+import axios from 'axios'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './index.css'
+import { Layout } from './layouts/Menu/Layout'
+import { Error } from './pages/Error/Error'
+import { Favorites } from './pages/Favorites/Favorites'
+import { Login } from './pages/Login/Login'
+import { Menu } from './pages/Menu/Menu'
+import { Movie } from './pages/Movie/Movie'
 
 const router = createBrowserRouter([
 	{
@@ -26,6 +26,7 @@ const router = createBrowserRouter([
 			{
 				path: '/movie/:id',
 				element: <Movie />,
+				errorElement:<>Ошибка...</>
 				loader: async ({ params }) => {
 					const { data } = await axios.get(
 						`https://search.imdbot.workers.dev/?tt=${params.id}`
