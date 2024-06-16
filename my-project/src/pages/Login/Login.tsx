790d68f8f74a26bdcd575b21413d4822756fb9a2
Yury Sidyakin <yurafikys@gmail.com>
2024-06-16 12:45:02 +0300

    fix: fix private router

--- my-project/src/pages/Login/Login.tsx
+++ my-project/src/pages/Login/Login.tsx
@@ -1,10 +1,12 @@
 import { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import InputName from '../../components/InputName/InputName';
 import Title from '../../components/Title/Title';
 
 export function Login() {
 	const [inputName, setInputName] = useState('');
+	const navigate = useNavigate();
 
 	const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setInputName(event.target.value);
@@ -13,6 +15,7 @@ export function Login() {
 	const addName = (event: any) => {
 		event.preventDefault();
 		localStorage.setItem('name', JSON.stringify(inputName));
+		navigate('/');
 	};
 
 	return (
